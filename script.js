1de4a9787788c7c0dab56240ba41ba9d44b7e91e
williycole <64283810+williycole@users.noreply.github.com>
2020-11-10 09:05:17 -0600

    Add functionality in for adding new tasks

--- script.js
+++ script.js
@@ -5,32 +5,35 @@ let editIdArray = [];
 let listItemsArray = [];
 let listItemId = 0;
 const taskList = document.querySelector('#task-ul');
-const userInput = document.querySelector('#user-input').value;
 ///////-->>>
 ///////-- NOTE: NEW CONCEPT
 ////Adds to page
 function addToPage (userInput){
-    const taskLi = `<li class="item-cntr">
-    <button class="task-finished">
-    <i class="far fa-check-square icon"></i>
-    </button>
+        const taskLi = `<li class="item-cntr">
+        <button class="task-finished">
+        <i class="far fa-check-square icon"></i>
+        </button>
 
-    <p class="task icon">${userInput}</p>
+        <p class="task icon">${userInput}</p>
 
-    <button class="edit-task">
-    <i class="fas fa-edit icon"></i>
-    </button>
+        <button class="edit-task">
+        <i class="fas fa-edit icon"></i>
+        </button>
 
-    <button class="delete-task">
-    <i class="far fa-trash-alt delete-task icon"></i>
-    </button>
-    </li>`
-    const position = "beforeend";
-    taskList.insertAdjacentHTML(position, taskLi);
-}
-///////-->>>
-//-----Add object to array list
-document.querySelector('#add-item').addEventListener("click", function(event){
+        <button class="delete-task">
+        <i class="far fa-trash-alt delete-task icon"></i>
+        </button>
+        </li>`
+        const position = "beforeend";
+        taskList.insertAdjacentHTML(position, taskLi);
+    }
+    ///////-->>>
+
+
+    //-----Add object to array list
+    document.querySelector('#add-item').addEventListener("click", function(event){
+        ////get user input on click
+        const userInput = document.querySelector('#user-input').value;
     if(listItemsArray.length >= 0){
         listItemsArray.push(////Update list with Item
             {
@@ -40,7 +43,8 @@ document.querySelector('#add-item').addEventListener("click", function(event){
                 trash: false
             });
 
-            console.log('list item works');
+            // console.log('list item works');
+            console.log(userInput);
             console.log(listItemsArray);
             console.log(listItemId);
             ////--Add Item to page list
