fda96148a5ddf16407e62db241e08a59f34e462e
williycole <64283810+williycole@users.noreply.github.com>
2020-11-15 09:08:29 -0600

    minor backup

--- script.js
+++ script.js
@@ -3,10 +3,9 @@
 ********
 **** 01. Gen Varialbes for Program & local storage load
 **** 02. Add HTML li to page
-**** 03. Delete/local storge handlers & check off li item
+**** 03. Delete, Check Off  & Rename li item
 **** 04. Rename Item
-**** 05. Remove ul from page, and reset array list
-**** 06. Add object to array list and push to page
+**** 05. Add object to array list and push to page
 ********
 ******/
 
@@ -30,9 +29,21 @@ if (savedData) {//// If there are any saved items, update  list
 =======================***/
 function addToPage (userInput){
     const taskLi = `<li class="item-cntr" >
-                        <p class="task icon">${userInput}</p>
+                        <!--
+                        <i class="editBtn fas fa-edit icon">
+                            <p class="task icon">${userInput}</p>
+                            <input type="text" name="edit-task-name"placeholder="edit task">
+                        </i>
+                        -->
+
+                        <form class="edit-task-name-form">
+                            <i class="editBtn fas fa-edit icon">
+                            <p class="task icon">${userInput}</p>
+                            </i>
+                            <input class="edit-task-name" type="text" name="edit-task-name"placeholder="edit task">
+                        </form>
+
                         <i class="doneBtn far fa-check-square icon"></i>
-                        <i class="editBtn fas fa-edit icon"></i>
                         <i class="trashBtn far fa-trash-alt  icon">
                     </li>`
     const position = "beforeend";
@@ -41,9 +52,9 @@ function addToPage (userInput){
 
 
 /***=======================
- ***03. Delete/local storge handlers & check off li item
+ ***03. Delete, Check Off  & Rename li item
  ///////-- NOTE: NEW CONCEPT
-  --uses preventDefault
+  --handles local storge removal
 =======================***/
 taskList.addEventListener('click', deleteCheck);
 function deleteCheck(e){
@@ -68,6 +79,16 @@ function deleteCheck(e){
         ////Updates html check off to local storage
         localStorage.setItem('savedToDoList', taskList.innerHTML);
     }
+
+
+ ////// Pick up right below here
+
+    ////Renames Item
+    if(item.classList[0] === "editBtn"){
+        const textBox = document.querySelector(".edit-task-name");
+        textBox.style.display = "flex"
+        console.log('found me')
+    }
 }
 
 
@@ -83,40 +104,7 @@ document.querySelector('#delete-list').addEventListener("click", function(event)
 
 
 /***=======================
- ***05. Rename Item
-  --
-  --uses preventDefault
-=======================***/
-// taskList.addEventListener('click', deleteCheck);
-// function deleteCheck(e){
-//     const item = e.target;
-//     console.log(e.target);
-//     ////Deletes Items
-//     if(item.classList[0] === "trashBtn"){
-//         const trashCheckIt = item.parentElement;
-//         trashCheckIt.classList.add("dipOut")
-//         ////trashCheckIt.remove()
-//         ////Arrow function to delay remove and update local storage
-//           setTimeout(delayDelete = () => {
-//               trashCheckIt.remove()
-//               ////Deletes html from local storage
-//               localStorage.setItem('savedToDoList', taskList.innerHTML);
-//             }, 600);
-//         }
-//     ////Checks Off Items
-//     if(item.classList[0] === "doneBtn"){
-//         const trashCheckIt = item.parentElement;
-//         trashCheckIt.classList.toggle('finished-task');
-//         ////Updates html check off to local storage
-//         localStorage.setItem('savedToDoList', taskList.innerHTML);
-//     }
-// }
-// console.log(`localStorage.listItemsArray----${localStorage.listItemsArray}`);
-//// console.log(`listItemsArray----${listItemsArray}`);
-
-
-/***=======================
- ***06. Add object to array list and push to page
+ ***05. Add object to array list and push to page
   --relies on 01. and 02.
   --uses preventDefault
   --saves to local storage
